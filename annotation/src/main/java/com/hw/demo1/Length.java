701cb4707c3135f917a103795e6af27aea76abb8
zhangyaqi02 <zhangyaqi02@58.com>
2023-03-27 14:05:37 +0800

    注解demo

--- annotation/src/main/java/com/hw/demo1/Length.java
+++ annotation/src/main/java/com/hw/demo1/Length.java
@@ -1,16 +1,14 @@
 package com.hw.demo1;
 
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 定义一个注解@Length
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Length {
     int min();
 
